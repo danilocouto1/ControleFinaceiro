07b3b2d12b6deb0aa6d95ec39183ebf1bd71ebaf
danilocouto1 <dansantosalfa@gmail.com>
2021-10-24 12:38:54 -0300

    Criando dados base da tabela

---
+++ src/data/categories.ts
@@ -0,0 +1,7 @@
+import { Category } from "../types/Category";
+
+export const categories: Category = {
+    comida: { title: 'a', color: 'red', expense: true},
+    roupa: { title: 'a', color: 'white', expense: true},
+    salario: { title: 'a', color: 'green', expense: false}
+}
---
+++ src/data/item.ts
@@ -0,0 +1,8 @@
+import { Item } from "../types/Item";
+
+export const items: Item[] = [
+    {date: new Date(2021, 9, 15), category: 'Comida', title: 'BK', value: 12.33},
+    {date: new Date(2021, 9, 15), category: 'Comida', title: 'Sushi', value: 55.50},
+    {date: new Date(2021, 9, 15), category: 'Roupas', title: 'Renner', value: 122.00},
+    {date: new Date(2021, 9, 15), category: 'Other', title: 'Shoppe', value: 25.00}
+];
