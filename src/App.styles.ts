87ccc21dfb4292628bf6974fd0821a28866c7bbb
danilocouto1 <dansantosalfa@gmail.com>
2021-10-24 12:37:26 -0300

    Definindo os estilos da pagina principal

---
+++ src/App.styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Container = styled.div``;
+export const Header = styled.div`
+    background-color: #2B99FF;
+    height: 150px;
+    text-align: center;
+`;
+export const HeaderText = styled.h1`
+    margin: 0;
+    padding: 0;
+    padding-top: 30px;
+    color: #FFFF; 
+`;
+export const Body = styled.div`
+    margin: auto;
+    max-width: 1500px;
+    margin-bottom: 60px;
+`;
